2918b3fafb84a65497d4dbc94a1214289cf2446b
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-17 14:42:09 +0700

    Nambah route untuk login, register, dll

---
+++ app/src/main/java/com/example/pam_project_medicat_009/uicontroller/Route.kt
@@ -0,0 +1,10 @@
+package com.example.pam_project_medicat_009.uicontroller
+
+object Route {
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val HOME_PASIEN = "home_pasien"
+    const val HOME_DOKTER = "home_dokter"
+    const val PROFIL_PASIEN = "profil_pasien"
+    const val FORM_KELUHAN = "form_keluhan"
+}
