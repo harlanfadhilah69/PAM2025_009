5e13a1f4b4cc800af9058fd0ed0fe040978cc797
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-17 14:41:09 +0700

    Nambah file data class untuk registerRequest

---
+++ app/src/main/java/com/example/pam_project_medicat_009/modeldata/RegisterRequest.kt
@@ -0,0 +1,8 @@
+package com.example.pam_project_medicat_009.modeldata
+
+data class RegisterRequest(
+    val nama: String,
+    val email: String,
+    val password: String,
+    val no_telp: String
+)
